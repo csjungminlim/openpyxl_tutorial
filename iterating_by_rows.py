8b896c262bedbef2a932eabb058766463a0b8a51
jungmin Lim <csjungminlim@gmail.com>
2018-07-11 15:08:03 +0900

    update

---
+++ iterating_by_rows.py
@@ -0,0 +1,25 @@
+# iterating-by_rows.property
+
+from openpyxl import Workbook
+
+book = Workbook()
+sheet = book.active
+
+rows = (
+    (88, 46, 57),
+    (89, 38, 12),
+    (23, 59, 78),
+    (56, 21, 98),
+    (24, 18, 43),
+    (34, 15, 67)
+)
+
+for row in rows:
+    sheet.append(row)
+
+for row in sheet.iter_rows(min_row=1, min_col=1, max_row=6, max_col=3):
+    for cell in row:
+        print(cell.value, end=" ")
+    print()
+
+book.save('iterbyrows.xlsx')
